61798025366e3c53a8e30e38ba60b32cbd3d0548
YermakA <ermak.080@mail.ru>
2023-12-31 20:20:21 +0800

    feat: tailwind init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import MyHello from './components/MyHello.vue'
+</script>
+
+<template>
+  <MyHello/>
+</template>
+
+<style scoped></style>
---
+++ src/components/MyHello.vue
@@ -0,0 +1,5 @@
+<template>
+  <button>hello </button>
+  <h2>hello world!!I'm  a component!
+  </h2>
+</template>
