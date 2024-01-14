48c367d6b4d3f9cc83cb016eb3d3a43e7643aea3
YermakA <ermak.080@mail.ru>
2024-01-14 19:20:03 +0800

    feat: Home-page add

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,9 @@
 <script setup>
-import { onMounted, ref, watch, reactive } from 'vue'
-import getItems from '@/services/get-items'
-import Search from './components/Search.vue'
-import TheHeader from './components/TheHeader.vue'
-import CardList from './components/CardList.vue'
-
-import Drawer from './components/Drawer.vue'
-import fetchItems from '@/services/fetch-items'
-const drawerIsOpen = ref(false)
-const items = ref([])
-const filters = reactive({
-  sortBy: '',
-  searchQuery: ''
-})
-watch(filters, () => fetchItems(filters, items))
-onMounted(() => getItems(items))
-
-const onChangeSelect = (e) => {
-  filters.sortBy = e.target.value
-  console.log(e.target.value)
-}
-const onChangeInputSearch = (e) => {
-  let timeout
-  const changeSearch = () => {
-    filters.searchQuery = e.target.value
-  }
-  clearInterval(timeout)
-  timeout = setTimeout(changeSearch, 1000)
-}
+import Home from '@/pages/Home.vue'
 </script>
 
 <template>
-  <Drawer v-if="drawerIsOpen" />
-  <div class="bg-white w-4/5 mx-auto h-auto rounded-3xl shadow-xl mt-14 pb-12 mb-16">
-    <TheHeader />
-    <Search :onChangeSelect="onChangeSelect" :on-change-input-search="onChangeInputSearch" />
-    <CardList :items="items" />
-  </div>
+  <Home />
 </template>
 
 <style scoped></style>
