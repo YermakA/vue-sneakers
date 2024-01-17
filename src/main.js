9ce456c6c6a841d51e6f1b93fd7e845f3b5ecf82
YermakA <ermak.080@mail.ru>
2024-01-17 17:19:41 +0800

    feat: add vueRouter

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import './assets/main.css'
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 import { createApp } from 'vue'
+const Home = { template: './pages/Home.vue' }
+import { VueRouter } from 'vue-router'
 import App from './App.vue'
 
+const routes = [
+  { path: '/', component: Home }
+  //{ path: '/favorites', component: Favorites }
+]
+
+const router = VueRouter.createRouter({
+  history: VueRouter.creaWebhistory(),
+  routes
+})
+
 const app = createApp(App)
+app.use(router)
 app.use(autoAnimatePlugin)
 app.mount('#app')
